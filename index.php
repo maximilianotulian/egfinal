86d8ac8bd389642022eabd105e11530fff1675f6
Joaquin Diaz <joadiaz_93@outlook.com>
2016-05-12 20:47:15 -0300

    Ejemplos de paralax y ruteo

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-  echo 'Index! Prueba';
+
+    $pagina = $_GET['pagina'];
+    $archivo = 'views/'.$pagina.'.php';
+
+    if(file_exists($archivo)){
+        include $archivo;
+    } else {
+        include 'views/home.php';
+    }
+
  ?>
+
+<div class="" style="height:500px;">
+asd
+</div>
+<div class="" style="background-image:url(trees-background.jpg); height:500px;background-attachment:fixed">
+
+</div>
+<div class="" style="height:500px;">
+asd
+</div>
