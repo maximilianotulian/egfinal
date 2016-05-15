62e061069173019206923bc00b225b12150164bc
Max Tulian <maxtulian93@hotmail.com>
2016-05-15 18:59:22 -0300

    finish with admin panel layout

--- admin/views/common/sidebar.php
+++ admin/views/common/sidebar.php
@@ -1,18 +1,15 @@
 <div class="admin--sidebar aside-nav">
 
-    <div class="admin--sidebar-header">
-        <span class="">
-
-            <img class="" src="http://0.gravatar.com/avatar/0eae3b79fc7792e26ca50d66dd76bfdc?s=50&d=monsterid&r=g" />
-            <a class="">Dario Diaz</a>
-        </span>
+    <div class="header">
+        <img class="header--avatar" src="http://0.gravatar.com/avatar/0eae3b79fc7792e26ca50d66dd76bfdc?s=50&d=monsterid&r=g" />
+        <a class="header--avatar-text">Dario Diaz</a>
     </div>
 
-    <div class="admin--sidebar-options">
-        <ul>
-            <li><a class="admin--sidebar-text" href="/admin/news">Agregar Noticia</a></li>
-            <li><a class="admin--sidebar-text" href="/admin/news">Editar Noticia</a></li>
-            <li><a class="admin--sidebar-text" href="/admin/news">Eliminar Noticia</a></li>
+    <div class="options">
+        <ul class="options--list">
+            <li><a class="options--list-item" href="/admin/news">Agregar Noticia</a></li>
+            <li><a class="options--list-item" href="/admin/news">Editar Noticia</a></li>
+            <li><a class="options--list-item" href="/admin/news">Eliminar Noticia</a></li>
         </ul>
     </div>
 
