ae04ce4163b0ba730d2e3357a2d0d6433c23eee0
Max Tulian <maxtulian93@hotmail.com>
2016-05-14 21:15:19 -0300

    start with admin layout

--- views/home.php
+++ views/home.php
@@ -1,4 +1,4 @@
-<div class="home home--baner banner-container">
+<div class="home home--baner banner--container">
 
     <?php $backgroundName="background1"; ?>
     <div class="banner" style="background-image: url(<?php echo '/sources/photos/'.$backgroundName.'.jpg'; ?>);" >
@@ -6,55 +6,12 @@
         <div class="section">
             <div class="container">
                 <br><br>
-                <h1 class="banner-title">Lorem ipsum</h1>
+                <h1 class="banner--title">Lorem ipsum</h1>
                 <div class="row center">
-                    <h5 class="banner-subtitle col s12 light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+                    <h5 class="banner--subtitle col s12 light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
                 </div>
                 <br><br>
             </div>
         </div>
-    </div>
-</div>
-
-
-<div class="container">
-    <div class="section">
-
-        <!--   Icon Section   -->
-        <div class="row">
-            <div class="col s12 m4">
-                <div class="icon-block">
-                    <h2 class="center brown-text">
-                        <i class="icon material-icons">flash_on</i>
-                    </h2>
-                    <h5 class="center">News</h5>
-
-                    <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </div>
-
-            <div class="col s12 m4">
-                <div class="icon-block">
-                    <h2 class="center brown-text">
-                        <i class="icon material-icons">group</i>
-                    </h2>
-                    <h5 class="center">News</h5>
-
-                    <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </div>
-
-            <div class="col s12 m4">
-                <div class="icon-block">
-                    <h2 class="center brown-text">
-                        <i class="icon material-icons">settings</i>
-                    </h2>
-                    <h5 class="center">News</h5>
-
-                    <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </div>
-        </div>
-
     </div>
 </div>
--- views/news.php
+++ views/news.php
@@ -1,14 +1,14 @@
 <!-- banner section -->
-<div class="news news--baner banner-container">
+<div class="news news--baner banner--container">
     <?php $backgroundName="background2"; ?>
     <div class="banner" style="background-image: url(<?php echo '/sources/photos/'.$backgroundName.'.jpg'; ?>);" >
 
         <div class="section">
             <div class="container">
                 <br><br>
-                <h1 class="banner-title">Lorem ipsum</h1>
+                <h1 class="banner--title">Lorem ipsum</h1>
                 <div class="row center">
-                    <h5 class="banner-subtitle col s12 light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+                    <h5 class="banner--subtitle col s12 light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
                 </div>
                 <br><br>
             </div>
