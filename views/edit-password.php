307ece5775c18dcadfebdc9d8706599018217853
Max Tulian <maxtulian93@hotmail.com>
2016-05-21 22:43:52 -0300

    update subject and replace delete-account for view-subjects

--- views/edit-password.php
+++ views/edit-password.php
@@ -1,32 +1,34 @@
 <div class="row profile--form">
-    <span class="profile--form-title">Cambiar contraseña</span>
-    <form class="col s12 profile--form">
+    <div class="col s12">
+        <div class="row">
+            <form class="col s12 profile--form" method="post" enctype="multipart/form-data" action="/controllers/edit.profile.php">
+                <h4>Cambiar contraseña</h4>
+                <div class="row profile--form-row">
+                    <div class="input-field col s12 profile--form-row-element">
+                        <input id="old_password" type="text" class="validate" name="old_password">
+                        <label for="old_password">Contraseña anterior</label>
+                    </div>
+                </div>
 
-        <div class="row profile--form-row">
-            <div class="input-field col s12 profile--form-row-element">
-                <input id="email" type="email" class="validate" name="email" required="required">
-                <label for="email">Email</label>
-            </div>
+                <div class="row profile--form-row">
+                    <div class="input-field col s12 l6 profile--form-row-element">
+                        <input id="new_password" type="text" class="validate" name="new_password">
+                        <label for="new_password">Contraseña nueva</label>
+                    </div>
+                    <div class="input-field col s12 l6 profile--form-row-element">
+                        <input id="repeat_new_password" type="text" class="validate" name="repeat_new_password">
+                        <label for="repeat_new_password">Repetir Contraseña nueva</label>
+                    </div>
+                </div>
+
+                <div class="row profile--form-row">
+                    <div class="col right">
+                        <button class="btn waves-effect waves-light profile--form-row-element-button" type="submit" name="action">
+                            Guardar
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
-
-        <div class="row profile--form-row">
-            <div class="input-field col s6 profile--form-row-element">
-                <input id="password" type="password" class="validate" name="password" required="required">
-                <label for="password">Nueva contraseña</label>
-            </div>
-
-            <div class="input-field col s6 profile--form-row-element">
-                <input id="repeat-password" type="password" class="validate" name="password_repeat" required="required">
-                <label for="repeat-password">Repetir Contraseña</label>
-            </div>
-        </div>
-
-        <div class="row profile--form-row">
-            <div class="col right">
-                <button class="btn waves-effect waves-light profile--form-row-element-button" type="submit" name="action">
-                    Guardar
-                </button>
-            </div>
-        </div>
-    </form>
+    </div>
 </div>
