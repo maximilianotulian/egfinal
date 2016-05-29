8fb9bbe419347be26d8c374dafe87ee4c9a6679c
Max Tulian <maxtulian93@hotmail.com>
2016-05-29 01:37:39 -0300

    star with validations

---
+++ sources/js/validation.js
@@ -0,0 +1,34 @@
+var regexEmail = /^(([a-zA-Z0-9])|([-]|[_]|[.]))+[@]([a-zA-Z0-9]){2,63}[.](([a-zA-Z0-9]){3,63})+$/;
+var regexOnlyNumber =/^[0-9]+$/;
+var regexOnlyText = /^[a-zA-Z]+$/;
+
+//Al menos seis caracteres, una minúscula, una mayuscula y un número.
+var regexPassword = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
+
+var isEqual = function (field1, field2) {
+    return (field1 === field2 || field1 == field2);
+};
+
+var isOnlyNumber = function (field) {
+    return regexOnlyNumber.test(field);
+}
+
+var isOnlyText = function (field) {
+    return regexOnlyText.test(field);
+};
+
+var isNullOrUndefined = function (field) {
+    return (field === undefined || field === null);
+};
+
+var validateMail = function (field) {
+    return regexEmail.test(field);
+};
+
+var validatePassWord = function (field) {
+    return regexPassword.test(field);
+};
+
+var validateContactSubmit = function () {
+    
+};
